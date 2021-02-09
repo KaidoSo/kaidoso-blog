30d9d52d332f30a1959117b5868d25ce69009f4a
KaidoSo <kaido.soomlais@gmail.com>
2021-02-09 15:38:14 +0200

    added blog preview and css for it

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'kaido', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'lillie', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'anon', id: 3 }
+    ])
+
     return ( 
-        <div className="Home">
-            <h2>Homepage</h2>
+        <div className="home">
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
